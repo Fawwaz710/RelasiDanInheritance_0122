3687a56133029579b1edf10ab8a2f52e2bbc99f1
FAWWAZ <fawwaznaufaldzulfikar@gmail.com>
2025-05-27 19:58:03 +0700

    membuat void cetak anak

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -20,3 +20,17 @@ class ibu {
     }
     void tambahAnak(anak*);
     void cetakAnak();
+};
+void ibu::tambahAnak(anak* pAnak){
+    daftar_anak.push_back(pAnak);
+}
+
+void ibu::cetakAnak() {
+    cout << "Daftar anak dari ibu \"" <<
+    this->nama << "\":n";
+    for (int i = 0; i < daftar_anak.size(); i++) {
+        cout << daftar_anak[i]->nama << endl;
+    }
+    cout << endl;
+}
+#endif
